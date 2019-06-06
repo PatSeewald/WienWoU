424d76f92b8691d4124f5c6a99388c5aa2822619
seewpatr <Cr203210>
2019-06-06 16:51:05 +0200

    karte funkt

--- www/js/app.js
+++ www/js/app.js
@@ -1,22 +1,45 @@
 // show error on mobile
-window.onerror = ( err ) => {
-  console.log( err );
+window.onerror = (error) => {
+  console.log(error);
   return true;
 }
 console.log = (s) => {
-    $( '#debug').html(s).css({padding:10});
+  $('#debug').html(s).css({ padding: 10 });
 }
 
-document.addEventListener( 'deviceready', () =>{
-  $( document ).ready( () =>{
-    console.log( 'DOM ready, alles ready...' );
+document.addEventListener('deviceready', () => {
+  $(document).ready(() => {
+    console.log('DOM ready, alles ready...');
     $('#debug')
-    .on('click', function() { $(this).empty().css({padding:0}); });
-    let karte = L.map( 'map' );
-    L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      .on('click', () => {
+        $('#debug').empty()
+          .css({
+            padding: 0
+          });
+      });
+    let karte = L.map('map');
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap'
-    } ).addTo( karte );
-    karte.setView( [ 48.21, 16.38 ], 12 );
-  
+    }).addTo(karte);
+    karte.setView([48.21, 16.38], 12);
+
+    $.ajax({
+      url: 'http://localhost:8287/getstations',
+      data: {},
+      method: 'post',
+      success: (data) => {
+        console.info(data);
+        for (const i in data.lines) {
+          let latlngs = [];
+          for (const j in data.lines[i].stations) {
+            latlngs.push([
+              data.lines[j].stations[i].lat,
+              data.lines[j].stations[i].lng,
+            ]);
+          }
+          let polyline = L.polyline(latlngs, { color: data.lines[k].color }).addTo(karte);
+        }
+      }
+    });
   });
 });
--- www/node/server.js
+++ www/node/server.js
@@ -16,7 +16,7 @@ let linien, steige, haltestellen;
 
 function loadCSV(url) {
   return new Promise((resolve, reject) => {
-    console.log(`Start load CSV: ${url} \n`)
+    console.log(`Start load CSV: ${url} \n`);
     request.get(url, (error, response, body) => {
       if (!error && response.statusCode == 200) {
         resolve(body);
@@ -40,29 +40,79 @@ function parseCSV(data) {
   });
 }
 
-function saveJSON() {
-  let color = {
-    U1: '#e20613',
-    U2: '#a762a3',
-    U3: '#ee7d00',
-    U4: '#009540',
-    U6: '#9d6930'
-  };
-  let data = {
-    lines: []
+var saveJSON = function() {
+
+  return new Promise( function( res,rej) {
+
+  var data = {lines:[]};
+  var c = {
+    U1:'#e20613',
+    U2:'#a762a3',
+    U3:'#ee7d00',
+    U4:'#009540',
+    U6:'#9d6930'
   };
 
-  for (let i in linien) {
-    if (linien[i][4] == 'ptMetro') {
+  var linienId =[];
+
+  //console.log( linien );
+  for (i in linien ) {
+    if ( linien[i][4] == 'ptMetro' ) {
+      linienId[ linien[i][0]*1 ] = data.lines.length;
       data.lines.push({
-        name: linien[i][1],
-        color: color[linien[i][1]],
-        stations: []
+        name:linien[i][1],
+        color:c[linien[i][1]],
+        stations:[]
       });
     }
   }
-  
-  console.log(data);
+  //console.log( linienId );
+  //console.log( steige );
+  for ( i in steige ) {
+    var j = linienId[ steige[i][1] ];
+    //console.log( 's'+steige[1] );
+    if ( typeof j == 'number' ) {
+        if ( data.lines[j].stations.indexOf( steige[i][2]) == -1 ) {
+          data.lines[j].stations.push(steige[i][2]);
+        }
+    }
+  }
+
+
+
+  var hs = [];
+  for ( i in haltestellen ) {
+
+    if ( isFinite(haltestellen[i][0]*1) ) {
+      hs[ haltestellen[i][0]*1 ] = [
+         haltestellen[i][3],  haltestellen[i][6], haltestellen[i][7]
+      ];
+    }
+  }
+
+
+
+  for (i in data.lines ) {
+    for (j in data.lines[i].stations ) {
+      var k = data.lines[i].stations[j];
+      data.lines[i].stations[j] = {
+        name: hs[k][0],
+        lat:hs[k][1],
+        lng:hs[k][2]
+      }
+    }
+  }
+
+  fs.writeFile( 'linien.json', JSON.stringify(data), function(err) {
+    if ( !err ) {
+      res();
+    } else {
+      rej();
+    }
+  } );
+
+  });
+
 }
 
 app.get('/createjson', (request, response) => {
@@ -100,7 +150,7 @@ app.get('/createjson', (request, response) => {
   response.end('linien.json created');
 });
 
-app.post('getstations', (request, response) => {
-  response.sendFile(`${__dirname} + /linien.json`);
+app.post('/getstations', (request, response) => {
+  response.sendFile(`${__dirname}/linien.json`);
 });
 
